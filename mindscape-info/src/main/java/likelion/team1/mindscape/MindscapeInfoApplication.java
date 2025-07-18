d93d30ca5b8f236c42b3cbe15b27b212d79816de
door JH <elogio.mi@gmail.com>
2025-07-18 16:19:30 +0900

    [FEAT] 백엔드 개발환경 세팅

---
+++ mindscape-info/src/main/java/likelion/team1/mindscape/MindscapeInfoApplication.java
@@ -0,0 +1,13 @@
+package likelion.team1.mindscape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindscapeInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindscapeInfoApplication.class, args);
+	}
+
+}
---
+++ mindscape-service/src/main/java/likelion/team1/mindscape/MindscapeServiceApplication.java
@@ -0,0 +1,13 @@
+package likelion.team1.mindscape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindscapeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindscapeServiceApplication.class, args);
+	}
+
+}
