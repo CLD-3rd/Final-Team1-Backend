5f134e701c039af2203e3ca366d99479cce7c284
Youngmin Jung <youngmin.jung@outlook.com>
2025-07-21 15:30:35 +0900

    feat(book): add book detail lookup using Kakao API
    - add BookResponse DTO
    - get first book item from response data from Kakao API
    - add temporary testing controller
    - add httpcomponents, json, mysql connector dependencies

--- mindscape-service/src/main/java/likelion/team1/mindscape/content/controller/ContentController.java
+++ mindscape-service/src/main/java/likelion/team1/mindscape/content/controller/ContentController.java
@@ -1,4 +1,20 @@
 package likelion.team1.mindscape.content.controller;
 
+import likelion.team1.mindscape.content.service.ContentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@RestController
 public class ContentController {
+    private final ContentService contentService;
+
+    @GetMapping("/test")
+    public ResponseEntity test() {
+        return ResponseEntity.ok(contentService.getBookDetails("해리 포터와 마법사의 돌"));
+    }
 }
--- mindscape-service/src/main/java/likelion/team1/mindscape/content/service/ContentService.java
+++ mindscape-service/src/main/java/likelion/team1/mindscape/content/service/ContentService.java
@@ -1,4 +1,78 @@
 package likelion.team1.mindscape.content.service;
 
+import likelion.team1.mindscape.content.dto.response.BookResponse;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.*;
+
+@Service
 public class ContentService {
+    @Value("${service.api.kakaobooks}")
+    private String kakaoApi;
+
+    public BookResponse getBookDetails(String title) {
+        String apiURL = "https://dapi.kakao.com/v3/search/book?query=";
+
+        try {
+            // set query and request url -> create url object
+            String query = URLEncoder.encode(title, "UTF-8");
+            String reqUrl = apiURL + query;
+            URL url = new URL(reqUrl);
+
+            // open http connection
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Authorization", "KakaoAK " + kakaoApi);
+
+            // 200 = get inputstream, else = get errorstream
+            int responseCode = connection.getResponseCode();
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(
+                            responseCode == 200 ? connection.getInputStream() : connection.getErrorStream()
+                    )
+            );
+
+            // read response line -> parse into jsonobject
+            JSONObject bookJson = new JSONObject(br.readLine());
+            // extract "documents" array (incl. all books w/ same title)
+            JSONArray bookInfo = bookJson.getJSONArray("documents");
+            // get first book from bookInfo
+            JSONObject book = bookInfo.getJSONObject(0);
+
+            // author = jsonarray type => extract authors and make it into string type
+            JSONArray authorsArray = book.getJSONArray("authors");
+            String authors = String.join(", ", authorsArray.toList().stream()
+                    .map(Object::toString)
+                    .toArray(String[]::new));
+
+            // make result into bookresponse
+            BookResponse bookResponse = new BookResponse(
+                    (String) book.get("title"),
+                    authors,
+                    (String) book.get("contents"),
+                    (String) book.get("thumbnail"));
+
+            return bookResponse;
+
+        } catch (UnsupportedEncodingException e) {
+            // encoding error
+            throw new RuntimeException(e);
+        } catch (MalformedURLException e) {
+            // url construction error
+            throw new RuntimeException(e);
+        } catch (ProtocolException e) {
+            // invalid http protocol error
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            // IO error during connection/data reading
+            throw new RuntimeException(e);
+        }
+    }
 }
