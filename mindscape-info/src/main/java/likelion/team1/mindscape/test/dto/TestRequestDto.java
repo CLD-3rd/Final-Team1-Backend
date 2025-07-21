6a38e53431c68c600ca7edbf11414b4f6a101d5d
yejipo <yejipo@google.com>
2025-07-21 15:27:13 +0900

    feat/test코드추가

---
+++ mindscape-info/src/main/java/likelion/team1/mindscape/test/dto/TestRequestDto.java
@@ -0,0 +1,12 @@
+package likelion.team1.mindscape.test.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TestRequestDto {
+    private Long userId;
+    private String userType;
+    private String typeDescription;
+}
---
+++ mindscape-info/src/main/java/likelion/team1/mindscape/user/service/TestService.java
@@ -0,0 +1,44 @@
+package likelion.team1.mindscape.user.service;
+
+import likelion.team1.mindscape.test.dto.TestRequestDto;
+import likelion.team1.mindscape.test.entity.Test;
+import likelion.team1.mindscape.test.repository.TestRepository;
+import likelion.team1.mindscape.user.entity.User;
+import likelion.team1.mindscape.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.stream.Collectors;
+import likelion.team1.mindscape.test.dto.TestResponseDto;
+
+@Service
+@RequiredArgsConstructor
+public class TestService {
+
+    private final TestRepository testRepository;
+    private final UserRepository userRepository;
+
+    public void saveTest(TestRequestDto dto) {
+        User user = userRepository.findById(dto.getUserId())
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+
+        Test test = Test.builder()
+                .user(user)
+                .userType(dto.getUserType())
+                .typeDescription(dto.getTypeDescription())
+                .build();
+
+        testRepository.save(test);
+    }
+    public List<TestResponseDto> getTestHistory(Long userId) {
+        List<Test> testList = testRepository.findByUser_UserIdOrderByCreatedAtDesc(userId);
+        return testList.stream()
+                .map(test -> TestResponseDto.builder()
+                        .testId(test.getTestId())
+                        .userType(test.getUserType())
+                        .typeDescription(test.getTypeDescription())
+                        .createdAt(test.getCreatedAt())
+                        .build())
+                .collect(Collectors.toList());
+    }
+}
