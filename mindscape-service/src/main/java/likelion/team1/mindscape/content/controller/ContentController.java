2bf23a6c05cbc2feabdc82092966fba0bf56f28f
Youngmin Jung <youngmin.jung@outlook.com>
2025-07-21 17:18:49 +0900

    feat(book): add music detail lookup using LastFM API - add MusicResponse DTO - add temporary testing controller - add httpcomponents, json, mysql connector dependencies

--- mindscape-service/src/main/java/likelion/team1/mindscape/content/controller/ContentController.java
+++ mindscape-service/src/main/java/likelion/team1/mindscape/content/controller/ContentController.java
@@ -1,4 +1,27 @@
 package likelion.team1.mindscape.content.controller;
 
+import likelion.team1.mindscape.content.dto.response.MusicResponse;
+import likelion.team1.mindscape.content.service.MusicService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@RestController
 public class ContentController {
+    private final MusicService musicService;
+
+    @GetMapping("/test/{artist}/{title}")
+    public ResponseEntity getMusicDetail(@PathVariable String artist, @PathVariable String title) {
+        try {
+            MusicResponse musicResponse = musicService.getMusicDetails(artist, title);
+            return ResponseEntity.ok(musicResponse);
+        } catch (IOException e) {
+            return ResponseEntity.internalServerError().body("Internal server error: " + e.getMessage());
+        }
+    }
 }
