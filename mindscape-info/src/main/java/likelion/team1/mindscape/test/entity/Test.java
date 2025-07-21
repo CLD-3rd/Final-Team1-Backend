87ac4ad3b868cc61da01c6b2d71f4f661fe07251
yhy0009 <ref0009@naver.com>
2025-07-21 13:39:42 +0900

    feat: 기초 디렉터리 구축 및 엔티티 클래스 생성

---
+++ mindscape-info/src/main/java/likelion/team1/mindscape/test/entity/Test.java
@@ -0,0 +1,33 @@
+package likelion.team1.mindscape.test.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "test")
+public class Test {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long testId;
+
+    private String userType; // 예: 적극 참여형
+
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    private String typeDescription;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId")
+    private likelion.team1.mindscape.user.entity.User user;
+}
