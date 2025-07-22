ba3de31b20da3c43e890abc67bb78faba3efd6cd
Rossenzii <jangmj80@naver.com>
2025-07-22 14:11:21 +0900

    [feat] contentService created

---
+++ mindscape-service/src/main/java/likelion/team1/mindscape/content/service/ContentService.java
@@ -0,0 +1,9 @@
+package likelion.team1.mindscape.content.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ContentService {
+}
