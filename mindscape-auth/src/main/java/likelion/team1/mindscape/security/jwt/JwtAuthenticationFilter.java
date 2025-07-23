1d26e6161e7c31aa2d61c16a0d89df4f6b308a53
door-JH <elogio.mi@gmail.com>
2025-07-24 02:52:19 +0900

    [FEAT] refreshToken 추가, 웹 잔여토큰 있을 시 로그인 안되는 현상 수정

--- mindscape-auth/src/main/java/likelion/team1/mindscape/security/jwt/JwtAuthenticationFilter.java
+++ mindscape-auth/src/main/java/likelion/team1/mindscape/security/jwt/JwtAuthenticationFilter.java
@@ -8,7 +8,10 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import likelion.team1.mindscape.dto.LoginRequestDTO;
+import likelion.team1.mindscape.entity.User;
+import likelion.team1.mindscape.repository.UserRepository;
 import likelion.team1.mindscape.service.PrincipalDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -25,6 +28,9 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
     //인증(Authentication)을 처리하는 핵심 인터페이스
     private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final JwtProperties jwtProperties;
+
 
 //    // 생성자에서 로그인 URL 설정
 //    public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
@@ -38,6 +44,8 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     // 클라이언트로부터 받은 인증 정보로 로그인을 시도
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        deleteExistingTokenCookies(response);
 
         // 1. HTTP 요청 본문을 DTO로 변환
         ObjectMapper mapper = new ObjectMapper();
@@ -66,22 +74,63 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
 
         PrincipalDetails principalDetails = (PrincipalDetails) authResult.getPrincipal();
+        User user = principalDetails.getUser();
 
-        String jwtToken = JWT.create()
+        deleteExistingTokenCookies(response);
+
+        // accessToken 생성
+        String accessToken = JWT.create()
                 .withSubject(principalDetails.getAccountId())
                 .withClaim("uid", principalDetails.getUser().getId())
                 .withClaim("uname", principalDetails.getUser().getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + JwtProperties.EXPIRATION_TIME))
-                .sign(Algorithm.HMAC512(JwtProperties.SECRET));
+                .withExpiresAt(new Date(System.currentTimeMillis() + jwtProperties.getACCESS_TOKEN_EXPIRATION()))
+                .sign(Algorithm.HMAC512(jwtProperties.getSECRET()));
+
+        // refreshToken 생성
+        String refreshToken = JWT.create()
+                .withSubject(principalDetails.getAccountId())
+                .withExpiresAt(new Date(System.currentTimeMillis() + jwtProperties.getREFRESH_TOKEN_EXPIRATION()))
+                .sign(Algorithm.HMAC512(jwtProperties.getSECRET()));
+
+        user.updateRefreshToken(refreshToken);
+        userRepository.save(user);
 
 
-        //jwt토큰 httponly 쿠키에 저장
-        Cookie cookie = new Cookie(JwtProperties.HEADER_STRING, jwtToken);
-        cookie.setHttpOnly(true);
-        cookie.setMaxAge(JwtProperties.EXPIRATION_TIME / 1000);
-        cookie.setPath("/");
-        cookie.setSecure(true);
+        //JWT 토큰 httponly 쿠키에 저장
+        Cookie accessTokenCookie = new Cookie(JwtProperties.ACCESS_TOKEN_STRING, accessToken);
+        accessTokenCookie.setHttpOnly(true);
+        accessTokenCookie.setPath("/");
+        //cookie.setSecure(true);
 
-        response.addCookie(cookie);
+
+        Cookie refreshTokenCookie = new Cookie(JwtProperties.REFRESH_TOKEN_STRING, refreshToken);
+        refreshTokenCookie.setHttpOnly(true);
+        refreshTokenCookie.setPath("/");
+
+
+        response.addCookie(accessTokenCookie);
+        response.addCookie(refreshTokenCookie);
+    }
+
+    // 기존 토큰 쿠키 삭제 메서드
+    private void deleteExistingTokenCookies(HttpServletResponse response) {
+        Cookie deleteAccessToken = new Cookie(JwtProperties.ACCESS_TOKEN_STRING, null);
+        Cookie deleteRefreshToken = new Cookie(JwtProperties.REFRESH_TOKEN_STRING, null);
+
+        // 쿠키 만료 설정
+        deleteAccessToken.setMaxAge(0);
+        deleteRefreshToken.setMaxAge(0);
+
+        // 쿠키 경로 설정 (기존 쿠키와 동일한 경로여야 삭제 가능)
+        deleteAccessToken.setPath("/");
+        deleteRefreshToken.setPath("/");
+
+        // 보안 설정
+        deleteAccessToken.setHttpOnly(true);
+        deleteRefreshToken.setHttpOnly(true);
+
+        // 쿠키 추가
+        response.addCookie(deleteAccessToken);
+        response.addCookie(deleteRefreshToken);
     }
 }
