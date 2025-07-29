dfc4c344ce3ade71f955135e9977bb1bcb2aaa99
ju0204 <p1106000@gmail.com>
2025-07-29 13:00:44 +0900

    [FIX] AZ 추가

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ module "subnet" {
   vpc_id               = module.vpc.vpc_id
   public_subnet_cidrs  = ["192.168.1.0/24"]
   private_subnet_cidrs = ["192.168.2.0/24", "192.168.3.0/24"]
-  azs                  = ["ap-northeast-2a"]
+  azs                  = ["ap-northeast-2a", "ap-northeast-2c"]
 }
 # eks
 module "eks" {
