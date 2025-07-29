57a9d9992e6993f94bdfcc8f776617ec5eafaebf
ju0204 <p1106000@gmail.com>
2025-07-29 16:08:50 +0900

    [fix]aws-auth.tf 수정

--- terraform/aws-auth.tf
+++ terraform/aws-auth.tf
@@ -1,3 +1,4 @@
+// aws‑auth.tf (루트)
 resource "kubernetes_config_map" "aws_auth" {
   metadata {
     name      = "aws-auth"
@@ -6,25 +7,24 @@ resource "kubernetes_config_map" "aws_auth" {
 
   data = {
     mapRoles = yamlencode([
+      // 1) 워커 노드 역할
       {
         rolearn  = module.iam.eks_node_role_arn
         username = "system:node:{{EC2PrivateDNSName}}"
         groups   = ["system:bootstrappers", "system:nodes"]
-      }
-    ])
-    mapUsers = yamlencode([
+      },
+      // 2) Bastion EC2 역할
       {
-        userarn  = module.iam.bastion_role_arn
+        rolearn  = module.iam.bastion_role_arn
         username = "bastion-admin"
         groups   = ["system:masters"]
-      }
+      },
     ])
+    // mapUsers 블록은 제거해주세요
   }
 
-  # ← EKS 모듈이 완전히 준비된 뒤에야 이 리소스를 생성
-    depends_on = [
-        module.eks,
-        module.iam                # iam 모듈이 완전히 적용된 뒤
-
+  depends_on = [
+    module.eks,
+    module.iam,
   ]
 }
