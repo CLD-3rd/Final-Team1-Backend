7a11ce982e1b4fbabdf50194090d7f42eaaad347
JongHoon Kim <ref0009@naver.com>
2025-07-30 10:37:16 +0900

    fix: providers.tf 파일 수정

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -37,6 +37,7 @@ data "aws_eks_cluster_auth" "eks" {
 
 
 provider "kubernetes" {
+  alias = "eks"
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority)
   token                  = data.aws_eks_cluster_auth.eks.token
