96e193e0689067317f4015180439711069c55f30
door-JH <elogio.mi@gmail.com>
2025-07-31 15:34:57 +0900

    [FIX] cors 프로덕션환경 기준으로 수정

--- mindscape-info/src/main/java/likelion/team1/mindscape/WebConfig.java
+++ mindscape-info/src/main/java/likelion/team1/mindscape/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 요청 경로에 대해
-                .allowedOrigins("http://localhost:3000/") // 리액트 개발 주소
+                .allowedOrigins("https://aws.thejymes.com") // 리액트 개발 주소
                 .allowedMethods("*") // GET, POST, PUT 등 모두 허용
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 인증 정보 포함 허용 (JWT 등)
