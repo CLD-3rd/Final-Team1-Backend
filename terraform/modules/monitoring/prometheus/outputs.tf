3d158fdc3202615a6432fab5167fcb347cf563e2
juhee <127922614+ju0204@users.noreply.github.com>
2025-08-05 17:41:45 +0900

    Update outputs.tf

--- terraform/modules/monitoring/prometheus/outputs.tf
+++ terraform/modules/monitoring/prometheus/outputs.tf
@@ -1,5 +1,5 @@
 output "prometheus_name" {
 # 삭제 필요
-value = var.enabled ? helm_release.prometheus[0].name : null
-#  value = helm_release.prometheus.name
+#value = var.enabled ? helm_release.prometheus[0].name : null
+  value = helm_release.prometheus.name
 }
