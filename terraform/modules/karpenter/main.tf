c157f4d64e75b606a708cc47f6b999af12dedf65
juhee <127922614+ju0204@users.noreply.github.com>
2025-08-06 14:43:55 +0900

    Update main.tf

--- terraform/modules/karpenter/main.tf
+++ terraform/modules/karpenter/main.tf
@@ -3,7 +3,7 @@ resource "helm_release" "karpenter" {
   repository       = "oci://public.ecr.aws/karpenter"
   chart            = "karpenter"
   version          = var.karpenter_version
-  namespace        = var.namespace
+  namespace        = "karpenter"
   create_namespace = false
   skip_crds        = false  # CRDs 자동 설치
   wait             = true
