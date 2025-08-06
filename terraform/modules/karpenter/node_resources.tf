e307fba2cb7362ea9e51dac77edf6f013fc2cfed
juhee <127922614+ju0204@users.noreply.github.com>
2025-08-06 09:13:20 +0900

    [fix] ec2nodeclass및 nodepool주석

--- terraform/modules/karpenter/node_resources.tf
+++ terraform/modules/karpenter/node_resources.tf
@@ -1,17 +1,17 @@
 # modules/karpenter/node_resources.tf
 
-resource "kubectl_manifest" "ec2nodeclass" {
-  depends_on = [helm_release.karpenter]
-  yaml_body  = templatefile("${path.module}/ec2nodeclass.yaml.tpl", {
-    cluster_name     = var.cluster_name,
-    instance_profile = var.instance_profile,
-    ubuntu_ami_id    = var.ubuntu_ami_id,
-  })
-}
+#resource "kubectl_manifest" "ec2nodeclass" {
+#  depends_on = [helm_release.karpenter]
+#  yaml_body  = templatefile("${path.module}/ec2nodeclass.yaml.tpl", {
+#    cluster_name     = var.cluster_name,
+#    instance_profile = var.instance_profile,
+#    ubuntu_ami_id    = var.ubuntu_ami_id,
+#  })
+#}
 
-resource "kubectl_manifest" "nodepool" {
-  depends_on = [helm_release.karpenter]
-  yaml_body  = templatefile("${path.module}/nodepool.yaml.tpl", {
-    cluster_name = var.cluster_name,
-  })
-}
+#resource "kubectl_manifest" "nodepool" {
+#  depends_on = [helm_release.karpenter]
+#  yaml_body  = templatefile("${path.module}/nodepool.yaml.tpl", {
+#    cluster_name = var.cluster_name,
+#  })
+#}
