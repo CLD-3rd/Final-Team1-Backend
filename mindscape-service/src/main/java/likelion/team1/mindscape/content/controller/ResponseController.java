b19f996ed6e766077c295777c793233abb529722
JongHoon Kim <ref0009@naver.com>
2025-08-08 15:09:36 +0900

    fix: 서비스앱 api 경로 수정

--- mindscape-service/src/main/java/likelion/team1/mindscape/content/controller/ResponseController.java
+++ mindscape-service/src/main/java/likelion/team1/mindscape/content/controller/ResponseController.java
@@ -18,7 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/test")
+@RequestMapping("/api/response")
 @RequiredArgsConstructor
 public class ResponseController {
 
